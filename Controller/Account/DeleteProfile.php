3126624458e7089d7d50ca3e86266fe442d9e5ab
karliuka <karliuka.vitalii@gmail.com>
2017-12-07 09:02:03 +0200

    delete action

--- Controller/Account/DeleteProfile.php
+++ Controller/Account/DeleteProfile.php
@@ -6,6 +6,14 @@
  */
 namespace Faonni\SocialLogin\Controller\Account;
 
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\DataObjectFactory;
+use Magento\Framework\Math\Random;
+use Magento\Customer\Model\Session;
+use Psr\Log\LoggerInterface;
+use Faonni\SocialLogin\Helper\Data as SocialLoginHelper;
+use Faonni\SocialLogin\Model\ProviderFactory;
+use Faonni\SocialLogin\Model\ProfileFactory;
 use Faonni\SocialLogin\Controller\Account\AbstractAccount;
 
 /**
@@ -13,6 +21,48 @@ use Faonni\SocialLogin\Controller\Account\AbstractAccount;
  */
 class DeleteProfile extends AbstractAccount
 {
+    /**
+     * SocialLogin profile
+     *
+     * @var \Faonni\SocialLogin\Model\Profile
+     */
+    protected $_profile; 
+    
+   /**
+     * Initialize controller
+     *
+     * @param Context $context
+     * @param SocialLoginHelper $helper
+     * @param ProviderFactory $providerFactory
+     * @param ProfileFactory $profile      
+     * @param DataObjectFactory $dataObjectFactory 
+     * @param Session $customerSession
+     * @param Random $mathRandom     
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        Context $context,
+        SocialLoginHelper $helper,
+        ProviderFactory $providerFactory,
+        ProfileFactory $profileFactory,
+        DataObjectFactory $dataObjectFactory,
+        Session $customerSession,
+        Random $mathRandom,
+        LoggerInterface $logger
+    ) {
+        $this->_profile = $profileFactory->create();
+        
+        parent::__construct(
+            $context,
+            $helper,
+            $providerFactory,
+            $dataObjectFactory,
+            $customerSession,
+            $mathRandom,
+            $logger
+        );
+    }
+    
     /**
      * Delete Action
      *
@@ -22,5 +72,26 @@ class DeleteProfile extends AbstractAccount
     public function execute()
     {
 		$id = $this->getRequest()->getParam('id', false);
+		if ($id) {
+            try {
+                $profile = $this->_profile->load($id);
+				if ($profile->getCustomerId() === $this->_session->getCustomerId()) {
+					$profile->delete();
+					$this->messageManager->addSuccess(
+						__('You deleted the profile.')
+					);
+				} else {
+					$this->messageManager->addError(
+						__('We can\'t delete the profile right now.')
+					);
+				}                
+            } catch (\Exception $exception) {
+                $this->messageManager->addError(
+					$exception->getMessage()
+				);
+            }		
+		} 
+		return $this->resultRedirectFactory->create()
+			->setPath('*/*/socialProfile');
     }
 }
