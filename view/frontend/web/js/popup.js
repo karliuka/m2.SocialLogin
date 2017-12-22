f064a7fa5f43c6ae28ee49fab3b8dd408810f3ac
karliuka <karliuka.vitalii@gmail.com>
2017-12-22 22:37:22 +0200

    js

--- view/frontend/web/js/popup.js
+++ view/frontend/web/js/popup.js
@@ -15,29 +15,69 @@ function($) {
          * Widget Config Option
          * @var {Object}
          */		
-		options: {},
-
+		options: {
+			popupId: 'SocialLoginPopup'
+		},
+		
+        /**
+         * Popup Object
+         * @var {Object}
+         */		
+		popup: null,
+		
 		/**
 		* Initialize Widget
 		* @returns {void}
 		*/		
 		_create: function() {
 			
-			var width = $(this.element).data('width');
-			var height = $(this.element).data('height');
-			var left = ($(document).width() - width)/2;
-			var top = ($(document).height() - height)/2;			
-			
+			var width = $(this.element).data('width'),
+				height = $(this.element).data('height'),
+				left = ($(document).width() - width)/2,
+				top = ($(document).height() - height)/2;			
+
+			$(this.element).click(function() {
+				this._createPopup(width, height, left, top);
+			}.bind(this));		
+		},
+		
+		/**
+		* Create Popup
+		* @param {float} width
+		* @param {float} height
+		* @param {float} left
+		* @param {float} top
+		* @returns {void}
+		*/		
+		_createPopup: function(width, height, left, top) {
+			this._reset();	
+			this.popup = window.open(
+				this._getUrl(), 
+				this.options.popupId, 
+				"width=" + width + ",height=" + height + ",left=" + left + ",top=" + top + ",location=yes,status=yes"
+			);		
+		},
+		
+		/**
+		 * Retrieve of the Popup Url		 
+		 * @return {string}	 
+		 */		
+        _getUrl: function() {
 			var url = $(this.element).attr('href');
 			url += (url.indexOf('?') >= 0 ? '&' : '?') + 'display=popup';
 			$(this.element).attr('href', 'javascript:void(false);');
-			
-			$(this.element).click(function() {
-				if (typeof faonniSocialLogin !== 'undefined') faonniSocialLogin.close();
-				else var faonniSocialLogin;
-				faonniSocialLogin = window.open(url, "faonniSocialLogin", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top + ",location=yes,status=yes");				  
-			});		
-		}
+			return url;
+        },
+        
+		/**
+		 * Reset Popup 
+		 * @return {void}	 
+		 */		
+        _reset: function() {
+			if (null !== this.popup) {
+				this.popup.close();
+			}
+        } 		
 	});
  
     return $.faonni.socialLogin;
